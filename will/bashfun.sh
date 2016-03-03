089faa617ef735a20d97650dfb4602f0239103f9
Kev@rasppi <rasppi@ksoft-creative-projects.co.uk>
2016-03-03 20:38:58 +0000

    bash functions

---
+++ will/bashfun.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# include these functions to make it easier
+
+function gitauth {
+killall ssh-agent
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/gitrsa
+}
+
+function gitupload {
+git push origin master
+}
+
+
