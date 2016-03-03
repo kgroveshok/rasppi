2f8279bcf61cbe04c87cdac84f366471e30d35c5
Kev@rasppi <rasppi@ksoft-creative-projects.co.uk>
2016-03-03 20:55:21 +0000

    Some changes

--- will/bashfun.sh
+++ will/bashfun.sh
@@ -9,8 +9,24 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/gitrsa
 }
 
+function ga {
+killall ssh-agent
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/gitrsa
+}
+
 function gitupload {
 git push origin master
 }
+
+function gu {
+git push origin master
+}
+
+function gc {
+git commit -a
+}
+
 
 gitauth
