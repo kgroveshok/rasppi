3a59617169dcb3449dcd23cc2438d0faea66c527
Kev On Laptop <kgroves@houseofkiya.co.uk>
2019-12-12 19:20:23 +0000

    Add joystick startup

--- kev/4wdrobot/start_joy.sh
+++ kev/4wdrobot/start_joy.sh
@@ -3,15 +3,15 @@
 
 # https://pimylifeup.com/raspberry-pi-playstation-controllers/
 
-#sudo ~/sixpair/sixpair
+sudo /root/sixpair/sixpair
 
 
-sudo bluetoothctl <<EOF
-trust 04:76:6E:6D:6C:FA
-trust 00:26:5C:82:3E:C7
-EOF
+#sudo bluetoothctl <<EOF
+#trust 04:76:6E:6D:6C:FA
+#trust 00:26:5C:82:3E:C7
+#EOF
 
 
 service bluetooth start
-sixad start
+sudo sixad --start
 
